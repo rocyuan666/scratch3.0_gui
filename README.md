4109ecf2be1c15f354050f3b11b536d2c632a46c
yuanpeng666 <yp66666@outlook.com>
2019-06-13 11:46:09 +0800

    scratch3.0 gui本地局域网访问

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# scratch3.0_gui
-scratch3.0_gui本地浏览器访问
+# 本地局域网建立scratch3.0-gui
+## 所需安装软件
+- git、node
+## 下载源码
+` git clone git@github.com:yuanpeng666/scratch3.0_gui.git`
+
+## 进入目录
+` cd scratch3.0_gui`
+## 依赖库安装
+` npm install`
+## 启动
+` npm start`
+- 出现compiled successfully（已成功编译）ok
+## PC端访问
+- 本地浏览器访问 ` http://127.0.0.1:8601/`或者 ` http://localhost:8601/`进入scratch3.0 GUI
+- 成功编译，服务开启，后请勿关闭Git Bash窗口（跑服务中...）
+## 手机||平板端访问
+- 前提是PC、手机、平板，设备必须处于同一局域网下。
+- pc端调出cmd输入 ` ipconfig` 回车，查看找到IPv4地址（比如是192.168.1.110），手机||平板端浏览器访问 ` 192.168.1.110:8601` 即可。
---
+++ test/integration/sounds.test.js
@@ -0,0 +1,135 @@
+import path from 'path';
+import SeleniumHelper from '../helpers/selenium-helper';
+
+const {
+    clickText,
+    clickXpath,
+    findByText,
+    findByXpath,
+    getDriver,
+    getLogs,
+    loadUri,
+    rightClickText,
+    scope
+} = new SeleniumHelper();
+
+const uri = path.resolve(__dirname, '../../build/index.html');
+
+let driver;
+
+describe('Working with sounds', () => {
+    beforeAll(() => {
+        driver = getDriver();
+    });
+
+    afterAll(async () => {
+        await driver.quit();
+    });
+
+    test('Adding a sound through the library', async () => {
+        await loadUri(uri);
+        await clickText('Sounds');
+
+        // Delete the sound
+        await rightClickText('Meow', scope.soundsTab);
+        await driver.sleep(500); // Wait a moment for context menu; only needed for local testing
+        await clickText('delete', scope.soundsTab);
+
+        // Add it back
+        await clickXpath('//button[@aria-label="Choose a Sound"]');
+        let el = await findByXpath("//input[@placeholder='Search']");
+        await el.sendKeys('meow');
+        await clickText('Meow', scope.modal); // Should close the modal
+
+        // Add a new sound
+        await clickXpath('//button[@aria-label="Choose a Sound"]');
+        el = await findByXpath("//input[@placeholder='Search']");
+        await el.sendKeys('chom');
+        await clickText('Chomp'); // Should close the modal, then click the sounds in the selector
+        await findByXpath("//input[@value='Chomp']"); // Should show editor for new sound
+
+        await clickXpath('//button[@title="Play"]');
+
+        await clickText('Louder');
+        await clickText('Softer');
+        await clickText('Faster');
+        await clickText('Slower');
+        await clickText('Robot');
+        await clickText('Echo');
+        await clickText('Reverse');
+
+        const logs = await getLogs();
+        await expect(logs).toEqual([]);
+    });
+
+    test('Adding a sound by surprise button', async () => {
+        await loadUri(uri);
+        await clickText('Sounds');
+        const el = await findByXpath('//button[@aria-label="Choose a Sound"]');
+        await driver.actions().mouseMove(el)
+            .perform();
+        await driver.sleep(500); // Wait for thermometer menu to come up
+        await clickXpath('//button[@aria-label="Surprise"]');
+        const logs = await getLogs();
+        await expect(logs).toEqual([]);
+    });
+
+    test('Duplicating a sound', async () => {
+        await loadUri(uri);
+        await clickText('Sounds');
+
+        await rightClickText('Meow', scope.soundsTab);
+        await clickText('duplicate', scope.soundsTab);
+        await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for error
+
+        // Make sure the duplicated sound is named correctly.
+        await clickText('Meow2', scope.soundsTab);
+
+        const logs = await getLogs();
+        await expect(logs).toEqual([]);
+    });
+
+    // Regression test for gui issue #1320
+    test('Switching sprites with different numbers of sounds', async () => {
+        await loadUri(uri);
+
+        // Add a sound so this sprite has 2 sounds.
+        await clickText('Sounds');
+        await clickXpath('//button[@aria-label="Choose a Sound"]');
+        await clickText('A Bass'); // Closes the modal
+
+        // Now add a sprite with only one sound.
+        await clickXpath('//button[@aria-label="Choose a Sprite"]');
+        await clickText('Abby'); // Doing this used to crash the editor.
+
+        await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for error
+
+        // Make sure the 'Oops' screen is not visible
+        const content = await driver.getPageSource();
+        expect(content.indexOf('Oops')).toEqual(-1);
+
+        const logs = await getLogs();
+        await expect(logs).toEqual([]);
+    });
+
+    test.only('Adding multiple sounds at the same time', async () => {
+        const files = [
+            path.resolve(__dirname, '../fixtures/movie.wav'),
+            path.resolve(__dirname, '../fixtures/sneaker.wav')
+        ];
+        await loadUri(uri);
+        await clickText('Sounds');
+        const el = await findByXpath('//button[@aria-label="Choose a Sound"]');
+        await driver.actions().mouseMove(el)
+            .perform();
+        await driver.sleep(500); // Wait for thermometer menu to come up
+        const input = await findByXpath('//input[@type="file"]');
+        await input.sendKeys(files.join('\n'));
+
+        await findByText('movie', scope.soundsTab);
+        await findByText('sneaker', scope.soundsTab);
+
+        const logs = await getLogs();
+        await expect(logs).toEqual([]);
+    });
+});
